4cba22203024746769fb7b4acad29baf31a68783
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2022-11-02 16:31:31 -0300

    More Things added for the Timeline

--- Assets/Scripts/TriggerScary.cs
+++ Assets/Scripts/TriggerScary.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class TriggerScary : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayableDirector timeline;
 
-    // Update is called once per frame
-    void Update()
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+
+            if (OneTimeTrigger == true)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
     }
 }
